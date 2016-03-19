76d4658e234f4596600daf2a4c15fbf4b34f4d16
Adam Hernandez <hernandezdavid88@utexas.edu>
2016-03-19 10:56:23 -0500

    Here we go

---
+++ app/src/androidTest/java/org/workersdefense/httpwww/proyectodefensalaboral/ApplicationTest.java
@@ -0,0 +1,13 @@
+***REMOVED***
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+***REMOVED***
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+***REMOVED***
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+***REMOVED***
+***REMOVED***
