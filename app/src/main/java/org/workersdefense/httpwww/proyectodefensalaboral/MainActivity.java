04aaf052a37a658979fafad164044e8d1ca25783
Adam Hernandez <hernandezdavid88@utexas.edu>
2016-03-24 21:26:37 -0500

    Attempting to divy out objects

--- app/src/main/java/org/workersdefense/httpwww/proyectodefensalaboral/MainActivity.java
+++ app/src/main/java/org/workersdefense/httpwww/proyectodefensalaboral/MainActivity.java
@@ -3,9 +3,11 @@
 ***REMOVED***
 import android.os.AsyncTask;
 ***REMOVED***
+import android.support.annotation.Nullable;
 ***REMOVED***
 import android.widget.Button;
 import android.widget.EditText;
+***REMOVED***
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -14,6 +16,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 
 public class MainActivity extends CustomWindow {
@@ -27,11 +30,9 @@ public class MainActivity extends CustomWindow {
         b1.setOnClickListener(new View.OnClickListener() {
         ***REMOVED***
             public void onClick(View v) {
-                lookupEmployer(v);
-//                Intent intent = new Intent();
-//                intent.setClass(MainActivity.this, Results.class);
-//
-//                startActivity(intent);
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, Results.class);
+                startActivity(intent);
         ***REMOVED***
     ***REMOVED***);
 
@@ -40,14 +41,20 @@ public class MainActivity extends CustomWindow {
         ***REMOVED***
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this, Details.class);
-
+                intent.setClass(MainActivity.this, Results.class);
                 startActivity(intent);
+                waitForResults();
         ***REMOVED***
     ***REMOVED***);
 ***REMOVED***
 
-    public void lookupEmployer(View view) {
+    public void waitForResults() {
+        TextView resultTextView = (TextView) findViewById(R.id.result_list);
+        while (resultTextView == null) {
+            System.out.println("Waiting");
+    ***REMOVED***
+***REMOVED***
+    public void lookupEmployerName(View view) {
         EditText employerEditText = (EditText) findViewById(R.id.emp_name);
         String employer_name = employerEditText.getText().toString();
 
@@ -57,39 +64,52 @@ public class MainActivity extends CustomWindow {
     ***REMOVED***
 ***REMOVED***
 
-    private class CallAPI extends AsyncTask<String, String, String> {
+    public void lookupEmployerPostal(View view) {
+        EditText employerEditText = (EditText) findViewById(R.id.zipcode);
+        String employer_postal = employerEditText.getText().toString();
 
-    ***REMOVED***
-        protected String doInBackground(String... params) {
-
-            String urlString = params[0]; // URL to call
-            String resultToDisplay = "";
-            searchTextResult result = null;
-            InputStream in = null;
-
-            // HTTP Get
-    ***REMOVED***
-
-                URL url = new URL(urlString);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                in = new BufferedInputStream(urlConnection.getInputStream());
-
-        ***REMOVED*** catch (Exception e) {
-                System.out.println(e.getMessage());
-                return e.getMessage();
-        ***REMOVED***
-            System.out.println(convertStreamToString(in));
-            return resultToDisplay;
-    ***REMOVED***
-
-        protected void onPostExecute(String result) {
-
+        if (employer_postal != null && !employer_postal.isEmpty()) {
+            String urlString = apiURL + "employer/postal?q=" + employer_postal;
+            new CallAPI().execute(urlString);
     ***REMOVED***
 ***REMOVED***
 
-    private class searchTextResult {
-        public String statusNbr;
-        public String hygieneResult;
+    public class Wrapper {
+        public TextView resultsTextView;
+        public String resultsToDisplay;
+***REMOVED***
+
+    private class CallAPI extends AsyncTask<String, String, Wrapper> {
+
+    ***REMOVED***
+        protected Wrapper doInBackground(String... params) {
+
+        String urlString = params[0]; // URL to call
+        InputStream in;
+        Wrapper w = new Wrapper();
+        w.resultsTextView = (TextView) findViewById(R.id.result_list);
+        // HTTP Get
+***REMOVED***
+
+            URL url = new URL(urlString);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            in = new BufferedInputStream(urlConnection.getInputStream());
+
+        ***REMOVED*** catch (Exception e) {
+                System.out.println(e.getMessage());
+                return w;
+        ***REMOVED***
+            w.resultsToDisplay = convertStreamToString(in);
+            return w;
+    ***REMOVED***
+
+        protected void onPostExecute(Wrapper w) {
+            setData(w.resultsToDisplay, w.resultsTextView);
+    ***REMOVED***
+***REMOVED***
+
+    private void setData(String data, TextView mTextView) {
+        mTextView.setText(data);
 ***REMOVED***
 
     private static String convertStreamToString(InputStream is) {
@@ -97,7 +117,7 @@ public class MainActivity extends CustomWindow {
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         StringBuilder sb = new StringBuilder();
 
-        String line = null;
+        String line;
 ***REMOVED***
             while ((line = reader.readLine()) != null) {
                 sb.append(line + "\n");
