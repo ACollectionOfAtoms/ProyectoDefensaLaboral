cf5e64170a287e7964f985fb0cbf0265370d4c92
Adam Hernandez <hernandezdavid88@utexas.edu>
2016-03-29 21:32:00 -0500

    Added collapsable list for each violation. BUG: Jose Morales Roofing doesn't appear to be parsed similar to others causing violation details to be erroneous (Total is correct however)

--- app/src/main/java/org/workersdefense/httpwww/proyectodefensalaboral/Details.java
+++ app/src/main/java/org/workersdefense/httpwww/proyectodefensalaboral/Details.java
@@ -3,7 +3,21 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+import java.util.List;
+import android.app.Activity;
+***REMOVED***
+***REMOVED***
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
 
+***REMOVED***
+***REMOVED***
 ***REMOVED***
 
 ***REMOVED***
@@ -11,10 +25,17 @@
 ***REMOVED***
 
 public class Details extends CustomWindow {
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
     private static final String VIOLATIONCOUNT = "numViolations";
+    int oshaViolationCount = 0;
+    int minWageViolationCount = 0;
+    int wageTheftViolationCount = 0;
 
 ***REMOVED***
 ***REMOVED***
@@ -25,7 +46,7 @@ public class Details extends CustomWindow {
         String postal;
         String address;
         String violationCount;
-        String violationData;
+        JSONArray violationData;
 
         TextView nameView = (TextView) findViewById(R.id.details_emp_name);
         TextView postalView = (TextView) findViewById(R.id.details_emp_postal);
@@ -38,16 +59,78 @@ public class Details extends CustomWindow {
         postal = intent.getStringExtra(POSTAL);
         address = intent.getStringExtra(ADDRESS);
         violationCount = intent.getStringExtra(VIOLATIONCOUNT);
-        violationData = intent.getStringExtra("violationData");
+
+***REMOVED***
+            violationData = new JSONArray(intent.getStringExtra("violationData"));
+            System.out.println(intent.getStringExtra("violationData"));
+            for (int i = 0; i < violationData.length(); i++) {
+                System.out.println(violationData.getJSONObject(i).get("type"));
+                if (violationData.getJSONObject(i).get("type").equals("OSHA")) {
+                    //oshaViolationCount += (int)violationData.getJSONObject(i).get("count");
+                    oshaViolationCount += 1;
+            ***REMOVED*** else if (violationData.getJSONObject(i).get("type").equals("TWC")) {
+                    //wageTheftViolationCount += (int)violationData.getJSONObject(i).get("count");
+                    wageTheftViolationCount += 1;
+            ***REMOVED*** else if (violationData.getJSONObject(i).get("type").equals("WHD")) {
+                    //minWageViolationCount += (int)violationData.getJSONObject(i).get("count");
+                    minWageViolationCount += 1;
+            ***REMOVED***
+        ***REMOVED***
+
+            violationDataView.setText("OSHA Violations: " + Integer.toString(oshaViolationCount) + "\n"
+                                      + "Wage Theft Violations: " + Integer.toString(wageTheftViolationCount) + "\n"
+                                      + "Minimum Wage Violations: " + Integer.toString(minWageViolationCount)
+            );
+    ***REMOVED*** catch (JSONException e) {
+***REMOVED***
+            String altViolationData = intent.getStringExtra("violationData");
+            violationDataView.setText(altViolationData);
+    ***REMOVED***
 
         nameView.setText(name);
         postalView.setText(postal);
         addressView.setText(address);
         violationCountView.setText(violationCount);
-        violationDataView.setText(violationData);
 
-        // TODO Add list of violation types with details for each row
+        // get the list view
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        //preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        //setting list adapter
+        expListView.setAdapter(listAdapter);
+
         this.title.setText(name);
         // this.icon.setImageResource(R.drawable.menu_info);
+***REMOVED***
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("OSHA Violations");
+        listDataHeader.add("Minimum Wage and Overtime Violations");
+        listDataHeader.add("Wage theft claims");
+
+        // Adding child data
+        List<String> osha = new ArrayList<String>();
+        osha.add(String.format("%d violations in the past 6 years\n " +
+                "Did not pay the total amount due to X workers in the past 6 years", oshaViolationCount));
+
+        List<String> minWage = new ArrayList<String>();
+        minWage.add(String.format("%d violations in the past 6 years\n " +
+                "Did not pay the total amount due to X workers in the past 6 years", minWageViolationCount));
+
+        List<String> wageTheft = new ArrayList<String>();
+        wageTheft.add(String.format("%d violations in the past 6 years\n " +
+                " X workers in the past 6 years", wageTheftViolationCount));
+
+        listDataChild.put(listDataHeader.get(0), osha); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), minWage);
+        listDataChild.put(listDataHeader.get(2), wageTheft);
 ***REMOVED***
 ***REMOVED***
---
+++ app/src/main/java/org/workersdefense/httpwww/proyectodefensalaboral/ExpandableListAdapter.java
@@ -0,0 +1,106 @@
+***REMOVED***
+
+***REMOVED***
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+***REMOVED***
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+***REMOVED***
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    // Adapted from http://www.androidhive.info/2013/07/android-expandable-list-view-tutorial/
+    private Context _context;
+    private List<String> _listDataHeader; // header titles
+    // child data in format of header title, child title
+    private HashMap<String, List<String>> _listDataChild;
+
+    public ExpandableListAdapter(Context context, List<String> listDataHeader,
+                                 HashMap<String, List<String>> listChildData) {
+        this._context = context;
+        this._listDataHeader = listDataHeader;
+        this._listDataChild = listChildData;
+***REMOVED***
+
+***REMOVED***
+    public Object getChild(int groupPosition, int childPosititon) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .get(childPosititon);
+***REMOVED***
+
+***REMOVED***
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+***REMOVED***
+
+***REMOVED***
+    public View getChildView(int groupPosition, final int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+
+        final String childText = (String) getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_item, null);
+    ***REMOVED***
+
+        TextView txtListChild = (TextView) convertView
+                .findViewById(R.id.lblListItem);
+
+        txtListChild.setText(childText);
+        return convertView;
+***REMOVED***
+
+***REMOVED***
+    public int getChildrenCount(int groupPosition) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .size();
+***REMOVED***
+
+***REMOVED***
+    public Object getGroup(int groupPosition) {
+        return this._listDataHeader.get(groupPosition);
+***REMOVED***
+
+***REMOVED***
+    public int getGroupCount() {
+        return this._listDataHeader.size();
+***REMOVED***
+
+***REMOVED***
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+***REMOVED***
+
+***REMOVED***
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.violation_list_group, null);
+    ***REMOVED***
+
+        TextView lblListHeader = (TextView) convertView
+                .findViewById(R.id.lblListHeader);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+
+        return convertView;
+***REMOVED***
+
+***REMOVED***
+    public boolean hasStableIds() {
+        return false;
+***REMOVED***
+
+***REMOVED***
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+***REMOVED***
+***REMOVED***
